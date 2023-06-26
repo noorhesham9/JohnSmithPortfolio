edfb1787b75824f009ccab129d32a7c383ba9893
norhs <norhsham02@gmail.com>
2023-06-26 08:57:07 +0300

    finish header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Header from "./components/header/Header";
+import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,58 @@
-import React from "react";
+import { useState } from "react";
 import "./header.css";
 const Header = () => {
+  const [show, setShow] = useState(false);
   return (
     <header className="header">
       <nav className="nav container">
-        <a href="index.html" className="nav__logo"></a>
-        <div className="nav__menu">
-          <ul className="nav__list grid"></ul>
+        <a href="index.html" className="nav__logo">
+          {" "}
+          smith
+        </a>
+        <div className={show ? "nav__menu show-menu" : "nav__menu"}>
+          <ul className="nav__list grid">
+            <li className="nav__item">
+              <a href="#home" className="nav__link">
+                <i className="uil uil-estate nav_icon"></i>
+                Home
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#about" className="nav__link">
+                <i className="uil uil-user nav_icon"></i> About
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#skills" className="nav__link">
+                <i className="uil uil-file-alt nav_icon"></i>
+                Skills
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#services" className="nav__link">
+                <i className="uil uil-briefcase-alt nav_icon"></i>
+                Services
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#portfolio" className="nav__link">
+                <i className="uil uil-scenery nav_icon"></i>
+                portfolio
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#contact" className="nav__link">
+                <i className="uil uil-message nav_icon"></i>
+                Contact
+              </a>
+            </li>
+          </ul>
+          <i
+            onClick={() => setShow(false)}
+            className="uil uil-times nav__colse"></i>
+        </div>
+        <div onClick={() => setShow(true)} className="nav__toggle">
+          <i className="uil uil-apps"></i>
         </div>
       </nav>
     </header>
