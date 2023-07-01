990b9caed7f142b6299f74401a320472a5d3e249
norhs <norhsham02@gmail.com>
2023-07-01 13:37:11 +0300

    finish about

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Header from "./components/header/Header";
 import "./App.css";
 import Home from "./components/home/home";
+import About from "./components/about/About";
+import Skills from "./components/skills/Skills";
 function App() {
   return (
     <>
       <Header />
       <main className="main">
         <Home />
+        <About />
+        <Skills />
       </main>
     </>
   );
---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,6 @@
+import "./skills.css";
+const Skills = () => {
+  return <section className="skills section" id="skills"></section>;
+};
+
+export default Skills;
